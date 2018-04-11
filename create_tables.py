ed5d669f0c51cc028accbc31e282640110b034c9
gutfeeling <dibyachakravorty@gmail.com>
2018-04-11 20:41:35 +0200

    added script for creating db tables

---
+++ create_tables.py
@@ -0,0 +1,53 @@
+import os
+
+from sqlalchemy import (create_engine, MetaData, Table, Column,
+    Integer, String, DateTime, Boolean, ForeignKey, 
+    )
+
+if __name__ == "__main__":
+
+    DATABASE_URL = os.environ["DATABASE_URL"]
+
+    engine = create_engine(DATABASE_URL, echo = False)
+    metadata = MetaData()
+    metadata.reflect(bind = engine)
+
+    block_table = Table("block", metadata,
+        Column("id", Integer, primary_key=True),
+        Column("block_number", Integer),
+        Column("timestamp", DateTime)
+        )
+
+    transaction_table = Table("transaction", metadata,
+        Column("id", Integer, primary_key=True),
+        Column("amount", Integer),
+        Column("gas_price", Integer),
+        Column("gas", Integer),
+        Column(
+            "from",
+            Integer,
+            ForeignKey("address.id", ondelete = "CASCADE"),
+            nullable=False,
+            ),
+        Column(
+            "to",
+            Integer,
+            ForeignKey("address.id", ondelete = "CASCADE"),
+            nullable=False,
+            ),
+        Column(
+            "block_id",
+            Integer,
+            ForeignKey("block.id", ondelete = "CASCADE"),
+            nullable=False,
+            )
+        )
+
+    address_table = Table("address", metadata,
+        Column("id", Integer, primary_key=True),
+        Column("hex", String(length = 42), unique = True, index = True),
+        # 0 means normal, 1 means smart contract
+        Column("type", Boolean),
+        )
+
+    metadata.create_all(engine)
